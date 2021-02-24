9db29a5265feb8ab6fc8df9d4f04757dd90866fe
Majkes <Majk3s@wp.pl>
2021-02-24 12:18:51 +0100

    java

---
+++ src/com/company/Zadanie3.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Podaj Stopnie");
+        int celcjusz = scanner.nextInt();
+
+        System.out.println ( (celcjusz*1.8) + 32);
+
+
+
+
+
+    }
+}
