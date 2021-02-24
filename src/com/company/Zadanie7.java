49cb4c06b3efa7077acb5ca539d16f326fb3a89a
Majkes <Majk3s@wp.pl>
2021-02-24 12:53:09 +0100

    java

---
+++ src/com/company/Zadanie7.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Zadanie7 {
+    public static void main(String[] args) {
+        double punktx1, punkty1, punktx2, punkty2;
+                //Math.pow();
+        //Math.sqrt();
+
+        double wynik = Math.sqrt(Math.pow(punktx2-punktx1), 2) + (Math.pow(punkty2-punkty1), 2)
+    }
+}
